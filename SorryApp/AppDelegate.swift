c36ee871c4367d51fe35a2c41cb454b699320c7f
Arvindram Krishnamoorthy <ArvindKrish@D-173-250-246-39.dhcp4.washington.edu>
2016-09-19 16:18:50 -0700

    Mostly done.

--- SorryApp/AppDelegate.swift
+++ SorryApp/AppDelegate.swift
@@ -16,9 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    let defaults = NSUserDefaults.standardUserDefaults()
-
-
+    let defaults = NSUserDefaults.standardUserDefaults()    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         
